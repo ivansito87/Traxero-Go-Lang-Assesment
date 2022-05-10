59bfc532de8ee9b9debc7fde69ea1ca42be85374
Ivan Rendon <ivansito87@gmail.com>
2022-05-09 23:16:43 -0400

    Fixes issue with token

--- back-end/cmd/api/middleware.go
+++ back-end/cmd/api/middleware.go
@@ -1,11 +1,50 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+var (
+	username = "abc"
+	password = "123"
+)
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		setupResponse(&w, r)
 
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(204)
+			return
+		}
+
+		u, p, ok := r.BasicAuth()
+		if !ok {
+			fmt.Println(r)
+			fmt.Println("Error parsing basic auth")
+			w.WriteHeader(401)
+			return
+		}
+		if u != username {
+			fmt.Printf("Username provided is correct: %s\n", u)
+			w.WriteHeader(401)
+			return
+		}
+		if p != password {
+			fmt.Printf("Password provided is correct: %s\n", u)
+			w.WriteHeader(401)
+			return
+		}
+		fmt.Printf("Username: %s\n", u)
+		fmt.Printf("Password: %s\n", p)
+		w.WriteHeader(200)
 		next.ServeHTTP(w, r)
 	})
 }
